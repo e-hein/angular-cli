49b77c87fa532e2fdbf9b30992bea452787648e3
Emanuel Hein <github@e-hein.de>
2017-03-30 09:41:49 +0200

    extended test blueprint

--- packages/@angular/cli/blueprints/component/files/__path__/__name__.component.spec.ts
+++ packages/@angular/cli/blueprints/component/files/__path__/__name__.component.spec.ts
@@ -1,25 +1,66 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { <%= classifiedModuleName %>Component } from './<%= dasherizedModuleName %>.component';
 
 describe('<%= classifiedModuleName %>Component', () => {
+  let fixture: ComponentFixture<<%= classifiedModuleName %>ParentComponent>;
   let component: <%= classifiedModuleName %>Component;
-  let fixture: ComponentFixture<<%= classifiedModuleName %>Component>;
+  let debugElement: DebugElement;
+  let html: any;
+
+  function updateComponent() {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+  }
+
+  function startComponent() {
+    updateComponent();
+    debugElement = query('<%= selector %>');
+    component = debugElement.componentInstance;
+    html = debugElement.nativeElement;
+  }
+
+  function query(
+    cssSelector: string,
+    container: DebugElement = fixture.debugElement
+  ): DebugElement {
+    return container.query(By.css(cssSelector));
+  }
+
+  function queryAll(
+    cssSelector: string,
+    container: DebugElement = fixture.debugElement
+  ): DebugElement[] {
+    return container.queryAll(By.css(cssSelector));
+  }
+
+  @Component({
+    template: `
+      <<%= selector %>></<%= selector %>>
+    `
+  })
+  class <%= classifiedModuleName %>ParentComponent {}
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ <%= classifiedModuleName %>Component ]
+      declarations: [
+        <%= classifiedModuleName %>Component,
+        <%= classifiedModuleName %>ParentComponent
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(<%= classifiedModuleName %>Component);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(<%= classifiedModuleName %>ParentComponent);
   });
 
-  it('should create', () => {
+  it('should create', fakeAsync(() => {
+    startComponent();
+
     expect(component).toBeTruthy();
-  });
+  }));
 });
